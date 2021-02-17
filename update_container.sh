b4e8e9635fb9f4c1a4b41503c1c4fc4f389b4aa0
apwiggins <apwiggins@users.noreply.github.com>
2021-02-17 07:37:15 -0400

    Create update_container.sh

---
+++ update_container.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# usage: update_container.sh [container_name] [container_image]
+
+name=$1
+image=$2
+
+echo "==> stopping "$name
+sudo docker stop $name
+
+echo "==> removing old "$name" container"
+sudo docker rm $name
+
+echo "==> removing old images"
+sudo docker image prune -f
+
+echo "==> removing old volumes"
+sudo docker volume rm `docker volume ls -q -f dangling=true`
+
+echo "==> pulling new "$name" container"
+sudo docker pull $image
