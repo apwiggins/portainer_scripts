29a2dc37666078924b8f3e7cc5ef0d9686c622b7
apwiggins <apwiggins@users.noreply.github.com>
2024-08-26 15:45:52 -0300

    Update update_apt_snap_flatpak.sh

--- update_apt_snap_flatpak.sh
+++ update_apt_snap_flatpak.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 
+set -e
+
+ping -c 1 8.8.8.8 > /dev/null 2>&1 || { echo "No internet connection"; exit 1; }
+
 # update all the repositories - apt, snap, flatpak
 # auto-apply changes
-
 sudo apt update && \
     sudo apt dist-upgrade -y && \
     sudo apt autoremove -y && \
     sudo snap refresh && \
-    sudo flatpak update -y  && \
-    /snap/bin/rustup update 'stable-x86_64-unknown-linux-gnu' && \
-    snap-store --quit && sudo snap refresh snap-store
-    
+    sudo flatpak update -y
+
+# Quit and refresh Snap Store
+snap-store --quit || { echo "Failed to quit snap-store"; }
+sudo snap refresh snap-store
+
+# Update Rust channel
+rust_channel='stable-x86_64-unknown-linux-gnu'
+RUSTUP=$HOME/.cargo/bin/rustup
+
+$RUSTUP update $rust_channel
+
 # no auto-apply
 
 #sudo apt update && \
